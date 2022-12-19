2df82f3ff6701440caf1482916d46a6f50bc09fc
Tatyana Larina <jerrymurr@ya.ru>
2022-12-19 14:41:03 +0300

    second commit

---
+++ src/main/java/ru/netology/aviatikets/Ticket.java
@@ -0,0 +1,69 @@
+package ru.netology.aviatikets;
+
+public class Ticket implements Comparable<Ticket> {
+    private int ID;
+    private int cost;
+    private String departure;
+    private String arrival;
+    private int time;
+
+    public Ticket(int ID, int cost, String departure, String arrival, int time) {
+        this.ID = ID;
+        this.cost = cost;
+        this.departure = departure;
+        this.arrival = arrival;
+        this.time = time;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public void setDeparture(String departure) {
+        this.departure = departure;
+    }
+
+    public String getArrival() {
+        return arrival;
+    }
+
+    public void setArrival(String arrival) {
+        this.arrival = arrival;
+    }
+
+    public int getTime() {
+        return time;
+    }
+
+    public void setTime(int time) {
+        this.time = time;
+    }
+
+
+    @Override
+    public int compareTo(Ticket o) {
+        if (this.cost < o.cost) {
+            return -1;
+        } else if (this.cost > o.cost) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ src/main/java/ru/netology/aviatikets/TicketManager.java
@@ -0,0 +1,28 @@
+package ru.netology.aviatikets;
+
+import static java.util.regex.Pattern.matches;
+
+public class TicketManager {
+    private TicketRepository repo;
+
+    public TicketManager(TicketRepository repo) {
+        this.repo = repo;
+    }
+
+    public Ticket[] findAll(String from, String to) {
+        Ticket[] result = new Ticket[0];
+        for (Ticket ticket : repo.getTickets()) {
+            if (matches(ticket.getDeparture(), from)) {
+                if (matches(ticket.getArrival(), to)) {
+                    Ticket[] tmp = new Ticket[result.length + 1];
+                    for (int i = 0; i < result.length; i++) {
+                        tmp[i] = result[i];
+                    }
+                    tmp[tmp.length - 1] = ticket;
+                    result = tmp;
+                }
+            }
+        }
+        return result;
+    }
+}
